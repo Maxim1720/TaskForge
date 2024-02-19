257df08743529c54a2faf9d26dc1f2e68d177926
maksim1708 <musiev_maksim@mail.ru>
2024-02-19 14:45:51 +0500

    To edit TokenResponser: adding method 'createResponseWithCookie'

--- backend/app/Service/TokenResponser.php
+++ backend/app/Service/TokenResponser.php
@@ -2,15 +2,30 @@
 
 namespace App\Service;
 
+use Illuminate\Http\JsonResponse;
+
 class TokenResponser
 {
 
+    private static $tokenTTL = 60;
+
     public static function createResponse($token)
+    {
+        return self::responseWithToken($token);
+    }
+
+    public static function createResponseWithCookie($token)
+    {
+        $cookie = cookie("auth-token", $token, self::$tokenTTL);
+        return self::responseWithToken($token)->cookie($cookie);
+    }
+
+    private static function responseWithToken($token): JsonResponse
     {
         return response()->json([
             'accessToken' => $token,
             'tokenType' => 'bearer',
-            'expiresIn' => auth()->factory()->getTTL() * 60
+            'expiresIn' => auth()->factory()->getTTL() * self::$tokenTTL
         ]);
     }
 }
